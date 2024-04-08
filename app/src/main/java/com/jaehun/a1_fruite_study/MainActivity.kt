fad44f26c6c5fc8303124d700ce02385eac1efb1
ilmntr <wogns9950@gmail.com>
2024-04-08 19:02:37 +0900

    first commit

---
+++ app/src/main/java/com/jaehun/a1_fruite_study/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.jaehun.a1_fruite_study
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val image1 = findViewById<ImageView>(R.id.fruit_image_1)
+        image1.setOnClickListener {
+            Toast.makeText(this, "1번 사진 클릭", Toast.LENGTH_LONG).show()
+        }
+
+
+    }
+}
